9967afe049dce2df5539cc1a57583fa99b0a8e27
iki1uc <entertrainer@web.de>
2026-05-30 23:32:44 +0200

    Update seeu_engine.js

--- seeu_engine.js
+++ seeu_engine.js
@@ -1,4 +1,3 @@
-// --- seeu_engine.js ---
 window.SEEU = window.SEEU || {};
 window.SEEU_PROJECTS = window.SEEU_PROJECTS || {};
 window.SEEU_CLONES = window.SEEU_CLONES || {};
@@ -13,7 +12,7 @@ window.SEEU_CREATE_CUBE = function(opts){
   }
 
   const cube = document.createElement("div");
-  cube.className = "cube state-online";
+  cube.className = "cube";
   cube.style.transform = "rotateX(0deg) rotateY(0deg)";
 
   const faces = window.SEEU_BUILD_FACES(projectId);
@@ -29,22 +28,14 @@ window.SEEU_CREATE_CUBE = function(opts){
     cube,
     rotX: 0,
     rotY: 0,
-
     applyImpulse(ix, iy){
       this.rotX += iy * 40;
       this.rotY += ix * 40;
       cube.style.transform = `rotateX(${this.rotX}deg) rotateY(${this.rotY}deg)`;
-    },
-
-    setState(state){
-      cube.classList.remove("state-online","state-busy","state-offline");
-      cube.classList.add(`state-${state}`);
     }
   };
 
   window.SEEU.applyImpulse = (x,y)=> inst.applyImpulse(x,y);
-  window.SEEU.setCubeState = (s)=> inst.setState(s);
-
   console.log("Cube erzeugt:", projectId);
   return inst;
 };
