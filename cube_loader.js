ddea3d85ffec922724538a7b99a08aacdf709ea2
iki1uc <entertrainer@web.de>
2026-05-30 23:32:19 +0200

    Update cube_loader.js

--- cube_loader.js
+++ cube_loader.js
@@ -1,23 +1,14 @@
-// --- cube_loader.js ---
-// Minimaler Loader für dein Cube-System
+window.SEEU_PROJECTS = window.SEEU_PROJECTS || {};
 
-// Projekt registrieren
-window.SEEU_REGISTER_PROJECT = function(id, data){
-  window.SEEU_PROJECTS[id] = data;
-  console.log("Projekt registriert:", id);
-};
-
-// Projekt laden (WICHTIG!)
 window.SEEU_LOAD_PROJECT = function(id){
-  // Falls Projekt nicht registriert wurde → Standard erzeugen
   if(!window.SEEU_PROJECTS[id]){
     window.SEEU_PROJECTS[id] = {
       id,
-      seed: Math.random().toString(36).slice(2),
+      seed: "test-seed",
       chunksBase: "default",
       version: "1.0"
     };
   }
-
   return window.SEEU_PROJECTS[id];
 };
+1
