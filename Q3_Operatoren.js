f231e68fdadd85b22731400da2ebe7d2385dfb83
iki1uc <entertrainer@web.de>
2026-05-31 22:33:35 +0200

    Update Q3_Operatoren.js

--- Q3_Operatoren.js
+++ Q3_Operatoren.js
@@ -1,1 +1,10 @@
+export const Q3_Operatoren = {
+  quadrant: "Q3",
+  role: "Operatoren",
+  pos: { x: 1, y: -1 },
+  angle: 225,
+  time: Date.now(),
+  state: "neutral",
+  access: "restricted"
+};
 
