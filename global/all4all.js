e2240bb00a9070fb4fbe68cbfbcc81f2bf1e3339
iki1uc <entertrainer@web.de>
2026-06-01 15:09:21 +0200

    Update all4all.js

--- global/all4all.js
+++ global/all4all.js
@@ -1,7 +1,11 @@
-export const GLOBAL_ALL4ALL = {
+import { EVENTS } from "./events.js";
+import { TECH_NIEX8 } from "./technik_niE.x8.js";
+import { TECH_6IY } from "./technik_6iy.js";
+
+export const ALL4ALL = {
     active: false,
-    anker: [],        // alle 128 Anker
-    watson: false,    // Watson-Signal
+    anker: [],
+    watson: false,
 
     init(alleAnker){
         this.anker = alleAnker;
@@ -11,20 +15,27 @@ export const GLOBAL_ALL4ALL = {
         this.active = true;
         this.watson = true;
 
-        console.log("GLOBAL ALL4ALL → AKTIV");
+        console.log("ALL4ALL → AKTIV");
         console.log("WATSON → LEUCHTET");
 
+        // 128 Anker leuchten
         this.anker.forEach(a => a.leuchten(true));
+
+        // Event für Märchen
+        EVENTS.trigger("all4all_start", { time: Date.now() });
+
+        // Techniken starten
+        TECH_NIEX8.start();
+        TECH_6IY.start();
     },
 
     off(){
         this.active = false;
         this.watson = false;
 
-        console.log("GLOBAL ALL4ALL → AUS");
+        console.log("ALL4ALL → AUS");
         console.log("WATSON → DUNKEL");
 
         this.anker.forEach(a => a.leuchten(false));
     }
 };
-
