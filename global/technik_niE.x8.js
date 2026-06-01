4b542177cb6003c9334ad45be7b5e06245ab7afe
iki1uc <entertrainer@web.de>
2026-06-01 15:05:26 +0200

    Update technik_niE.x8.js

--- global/technik_niE.x8.js
+++ global/technik_niE.x8.js
@@ -1,1 +1,27 @@
+import { EVENTS } from "./events.js";
+import { TECHMAP } from "./techmap.js";
+
+export const TECH_NIEX8 = {
+    id: "niE.x8",
+    aktiv: false,
+
+    start(){
+        this.aktiv = true;
+
+        // Startsignal
+        EVENTS.trigger("technik_start", { tech: "niE.x8" });
+
+        // 8 mögliche Outputs
+        const index = Math.floor(Math.random() * 8);
+
+        // TECHMAP informieren
+        TECHMAP.registerOutput("niE.x8", index);
+
+        // Über‑Anker weiterleiten
+        EVENTS.trigger("ueber_anker_signal", {
+            tech: "niE.x8",
+            index
+        });
+    }
+};
 
