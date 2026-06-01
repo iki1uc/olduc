20b7d082c8546c8695a2d09cd1c91b18208955fb
iki1uc <entertrainer@web.de>
2026-06-01 15:00:22 +0200

    Update maerchen.js

--- global/maerchen.js
+++ global/maerchen.js
@@ -1,1 +1,31 @@
+import { EVENTS } from "./events.js";
+
+export const MAERCHEN = {
+    pool: [
+        "Es war einmal ein Funke, der größer war als die Nacht.",
+        "Im Schatten einer alten Straße begann etwas Neues zu atmen.",
+        "Ein kleiner Fehler wurde zu einer großen Wahrheit.",
+        "Zwischen zwei Welten öffnete sich ein stiller Raum.",
+        "Ein verlorener Gedanke fand seinen Weg zurück.",
+        "Ein Herz erinnerte sich an etwas, das nie gesagt wurde.",
+        "Ein Schritt zu früh wurde zu einem Schritt genau richtig.",
+        "Ein Augenblick wurde zu einer ganzen Geschichte."
+    ],
+
+    r(){
+        return this.pool[Math.floor(Math.random()*this.pool.length)];
+    },
+
+    start(){
+        EVENTS.trigger("maerchen_output", {
+            text: this.r(),
+            time: Date.now()
+        });
+    }
+};
+
+// reagiert auf JEDE Technik
+EVENTS.on("technik_output", () => MAERCHEN.start());
+EVENTS.on("technik_start", () => MAERCHEN.start());
+EVENTS.on("ueber_anker_signal", () => MAERCHEN.start());
 
