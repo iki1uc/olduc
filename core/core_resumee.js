0472c9fa2295520477ee35ba5bf57630298fe529
iki1uc <entertrainer@web.de>
2026-06-01 03:14:55 +0200

    Update core_resumee.js

--- core/core_resumee.js
+++ core/core_resumee.js
@@ -1,1 +1,10 @@
+document.getElementById("resSchwester").onclick = ()=>{
+  alert("[Schwester spricht]\nIch sag’s dir, wie ich es sehe…");
+  halt("res_schwester");
+};
+
+document.getElementById("resKonseq").onclick = ()=>{
+  alert("[Konsequenz spricht]\nDu weißt, was das bedeutet…");
+  halt("res_konsequenz");
+};
 
