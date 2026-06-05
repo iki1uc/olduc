43e479851ae0e43584ae88c7c0577feece9fafea
iki1uc <entertrainer@web.de>
2026-06-06 00:48:48 +0200

    Update error_display.js

--- error_display.js
+++ error_display.js
@@ -1,1 +1,14 @@
+// error_display.js
+(function(){
+  const out = document.getElementById("out");
+
+  window.SEEU_ERROR_HANDLER = function(err){
+    const msg = String(err.message || err);
+    if (window.SEEU_ERROR_VISUAL && out){
+      window.SEEU_ERROR_VISUAL(msg, out);
+    } else if (out){
+      out.textContent = "Fehler:\n  " + msg;
+    }
+  };
+})();
 
