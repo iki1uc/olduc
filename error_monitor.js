2b38a0b75bb593463d41e82071200e3647f1da80
iki1uc <entertrainer@web.de>
2026-06-06 00:47:39 +0200

    Implement global error and rejection handler

--- error_monitor.js
+++ error_monitor.js
@@ -1,1 +1,17 @@
+// error_monitor.js
+(function(){
+  function forward(err){
+    if (window.SEEU_ERROR_HANDLER) {
+      window.SEEU_ERROR_HANDLER(err);
+    }
+  }
+
+  window.addEventListener("error", e => {
+    forward({ message: e.message, stack: e.error && e.error.stack });
+  });
+
+  window.addEventListener("unhandledrejection", e => {
+    forward({ message: String(e.reason), stack: e.reason && e.reason.stack });
+  });
+})();
 
